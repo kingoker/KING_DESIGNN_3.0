752594db612a0a2094288ea2ce79b99bb9933f4b
Akbar <zolotojznak@gmail.com>
2025-04-27 03:08:18 -0400

    google analytics added 2

--- app.vue
+++ app.vue
@@ -11,17 +11,13 @@
 </template>
 
 
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-L6XV805N79"></script>
 <script>
-export default {
-  head() {
-    return {
-      script: [
-        {
-          src: 'https://www.googletagmanager.com/gtag/js?id=G-L6XV805N79',
-          async: true,
-        },
-      ],
-    };
-  },
-};
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-L6XV805N79');
 </script>
